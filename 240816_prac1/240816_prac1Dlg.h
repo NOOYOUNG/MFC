794c881e471092c736ef7e09e8a62ce8f87b3862
2425343021 <chyooy23@gmail.com>
2024-08-19 20:03:42 +0900

    MessageBox

---
+++ 240816_prac1/240816_prac1Dlg.h
@@ -0,0 +1,40 @@
+﻿
+// 240816_prac1Dlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CMy240816prac1Dlg 대화 상자
+class CMy240816prac1Dlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CMy240816prac1Dlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MY240816_PRAC1_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+//	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+//	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonDblClk(UINT nFlags, CPoint point);
+	afx_msg void OnMButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+};
